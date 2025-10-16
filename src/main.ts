3b03e3e8890680763b295e92a3e6b6068ef199aa
QuestionMark-128 <aiqle@ucsc.edu>
2025-10-16 22:40:31 +0000

    (step 1 complete) Added a header to the page as well as a canvas with rounded corners and a shadow.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const title = document.createElement("h1");
+title.textContent = "You ain't Van Gogh";
+document.body.append(title);
+
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+canvas.width = 256;
+canvas.height = 256;
+document.body.append(canvas);
