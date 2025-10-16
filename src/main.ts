0a9e236fdeb44fa0e8c6621467c9556182db23d7
QuestionMark-128 <aiqle@ucsc.edu>
2025-10-16 23:46:39 +0000

    (step 2 complete) added drawing though event listeners and addedd a button that clears the canvas.

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,35 @@ canvas.id = "canvas";
 canvas.width = 256;
 canvas.height = 256;
 document.body.append(canvas);
+
+const clear = document.createElement("button");
+clear.textContent = "Clear";
+document.body.append(clear);
+
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+clear.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
