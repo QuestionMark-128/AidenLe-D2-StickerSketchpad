c6d7d0943adf4cbe8f5157cf8f3e442c46a25b24
QuestionMark-128 <aiqle@ucsc.edu>
2025-10-17 00:43:37 +0000

    (step 3 complete) created an array that stores the mouse positions. Added an observer for drawing-changed event

--- src/main.ts
+++ src/main.ts
@@ -17,27 +17,51 @@ document.body.append(clear);
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 const cursor = { active: false, x: 0, y: 0 };
 
+type Point = { x: number; y: number };
+const lines: Point[][] = [];
+let currentLine: Point[] | null = null;
+
+canvas.addEventListener("drawing-changed", () => {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+
+  lines.forEach((line) => {
+    if (line.length > 0) {
+      ctx.beginPath();
+      ctx.moveTo(line[0].x, line[0].y);
+      for (const lin of line) {
+        ctx.lineTo(lin.x, lin.y);
+      }
+      ctx.stroke();
+    }
+  });
+});
+
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+  currentLine = [];
+
+  currentLine.push({ x: cursor.x, y: cursor.y });
+  lines.push(currentLine);
+
+  canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
 canvas.addEventListener("mousemove", (e) => {
-  if (cursor.active) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
-  }
+  if (!cursor.active || !currentLine) return;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+  currentLine.push({ x: cursor.x, y: cursor.y });
+  canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
+  currentLine = null;
 });
 
 clear.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines.length = 0;
+  canvas.dispatchEvent(new Event("drawing-changed"));
 });
